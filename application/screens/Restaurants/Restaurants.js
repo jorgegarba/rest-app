8f671d4d7f9a7a637a8460f427cf2cbee3f27394
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-05-03 19:25:26 -0500

    Estilizando ListItem de Restaurantes

--- application/screens/Restaurants/Restaurants.js
+++ application/screens/Restaurants/Restaurants.js
@@ -3,33 +3,123 @@ import RestaurantEmpty from '../../components/Restaurant/RestaurantEmpty';
 import BackgroundImage from '../../components/BackgroundImage';
 import RestaurantAddButton from '../../components/Restaurant/RestaurantAddButton';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as firebase from 'firebase';
+import PreLoader from '../../components/PreLoader';
+
+import {FlatList,StyleSheet} from 'react-native';
+import {ListItem} from 'react-native-elements';
+
 export default class Restaurants extends Component {
 
-    static navigationOptions =  {
-        title: "Restaurants",
-        headerRight:(
-            <Icon name="home"
-                    style={{marginRight:20}}
-                    size={20}
-                    color="white"
-                    onPress={()=>{console.log("icono home");}}/>
-        ),
-        headerLeft:(
-            <Icon name="bars"
-                    style={{marginLeft:20}}
-                    size={20}
-                    color="white"
-                    onPress={()=>{console.log("icono bars");}}/>
-        ),
+    refRestaurants;
 
+    constructor() {
+        super();
+        this.state = {
+            restaurants: [],
+            loaded: false,
+            restaurant_logo: require('./../../../assets/images/restaurant.png')
+        };
+        this.refRestaurants = firebase.database().ref().child('restaurants');
     }
 
+    componentDidMount() {
+        this.refRestaurants.on('value', (data) => {
+            let restaurants = [];
+            data.forEach(fila => {
+                let objRestaurant = {
+                    id: fila.key,
+                    nombre: fila.val().nombre,
+                    capacidad: fila.val().capacidad,
+                    direccion: fila.val().direccion,
+                    descripcion: fila.val().descripcion,
+                    lat: fila.val().lat,
+                    lng: fila.val().lng,
+                };
+                restaurants.push(objRestaurant);
+            });
+            console.log(restaurants);
+            this.setState({
+                restaurants: restaurants,
+                loaded: true
+            })
+        });
+    }
+
+
+    static navigationOptions = ({ navigation }) => (
+        {
+            title: "Restaurants",
+            headerRight: (
+                <Icon name="home"
+                    style={{ marginRight: 20 }}
+                    size={20}
+                    color="white"
+                    onPress={() => { }} />
+            ),
+            headerLeft: (
+                <Icon name="bars"
+                    style={{ marginLeft: 20 }}
+                    size={20}
+                    color="white"
+                    onPress={() => { navigation.openDrawer(); }} />
+            ),
+        }
+    )
+
+
+    renderRestaurant(restaurant){
+        return(
+            <ListItem containerStyle={styles.item}
+                        titleStyle={styles.title}
+                        roundAvatar
+                        title={`${restaurant.nombre} (Capacidad: ${restaurant.capacidad})`}
+                        leftAvatar={{source:this.state.restaurant_logo}}
+                        rightIcon={{name:"arrow-right",
+                                    type:'font-awesome',
+                                    marginRight:10,
+                                    fontSize:15,
+                                    color:'white'}}
+                        >
+            </ListItem>
+        )
+    }
     render() {
-        return (
+
+        const { loaded, restaurants } = this.state;
+        if (loaded === false) {
+            return (<PreLoader></PreLoader>);
+        }
+        if (restaurants.length == 0) {
+            return (
+                <BackgroundImage source={require('./../../../assets/images/bg1.jpg')}>
+                    <RestaurantEmpty></RestaurantEmpty>
+                    <RestaurantAddButton></RestaurantAddButton>
+                </BackgroundImage>
+            )
+        }
+        return(
             <BackgroundImage source={require('./../../../assets/images/bg1.jpg')}>
-                <RestaurantEmpty></RestaurantEmpty>
-                <RestaurantAddButton></RestaurantAddButton>
+                <FlatList data={restaurants}
+                            renderItem={(data)=>{
+                                return this.renderRestaurant(data.item)
+                            }}>
+
+                </FlatList>
             </BackgroundImage>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        padding:5,
+        backgroundColor:'rgba(206,206,206,0.6)'
+    },
+    title:{
+        color:'white'
+    },
+    flechita:{
+        
+    }
+})
