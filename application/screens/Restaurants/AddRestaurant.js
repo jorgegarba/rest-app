a90415a0cec7f4cfa3eb8466c3e4924dec01e98b
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-05-04 09:57:45 -0500

    S/. 10.00 de commit

--- application/screens/Restaurants/AddRestaurant.js
+++ application/screens/Restaurants/AddRestaurant.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Restaurant, options } from './../../forms/crearRestaurant';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet, Alert, KeyboardAvoidingView } from 'react-native';
 import BackgroundImage from '../../components/BackgroundImage';
 import { Card } from 'react-native-elements';
 import t from 'tcomb-form-native';
@@ -10,63 +10,76 @@ import * as firebase from 'firebase';
 
 export default class AddRestaurant extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             restaurant: {
-                nombre:'',
-                descripcion:'',
-                direccion:'',
-                capacidad:0,
+                nombre: '',
+                descripcion: '',
+                direccion: '',
+                capacidad: 0,
             }
         }
     }
 
-    actualizarState(data){
+    actualizarState(data) {
         this.setState({
-            restaurant:data
+            restaurant: data
         })
     }
-    crearRestaurant(){
+    crearRestaurant() {
         // getValue() retorna un JSON con la información del formulario
         // SI y SOLO SI el formulario tiene los campos validados
         // en caso contrario, retorna "null"
         const validador = this.refs.form.getValue();
-        if(validador){
+        if (validador) {
             const pk = firebase.database().ref().child('restaurants').push().key;
-            
-            
+            firebase.database().ref().child('restaurants').child(pk).set({
+                nombre: this.state.restaurant.nombre,
+                descripcion: this.state.restaurant.descripcion,
+                direccion: this.state.restaurant.direccion,
+                capacidad: this.state.restaurant.capacidad
+            }).then(() => {
+                Alert.alert("Exito!", "El restaurant se ha creado");
+            }).catch(() => {
+                Alert.alert("Error", "Ocurrió un error fatal =(");
+            });
+
         }
     }
     render() {
         return (
             <BackgroundImage source={require('./../../../assets/images/bg1.jpg')}>
-                <View style={styles.container}>
-                    <Card title="Agregar un Restaurant">
-                        <View>
-                            <Form ref="form"
+                <KeyboardAvoidingView behavior="padding">
+
+                    <View style={styles.container}>
+                        <Card title="Agregar un Restaurant">
+                            <View>
+                                <Form ref="form"
                                     type={Restaurant}
                                     options={options}
                                     value={this.state.restaurant}
-                                    onChange={(data)=>{this.actualizarState(data)}}>
-                            </Form>
-                        </View>  
-                        <AppButton
+                                    onChange={(data) => { this.actualizarState(data) }}>
+                                </Form>
+                            </View>
+                            <AppButton
                                 bgColor="rgba(255,38,74,0.9)"
                                 title="Agregar"
                                 action={this.crearRestaurant.bind(this)}
                                 iconName="plus"
                                 iconSize={30}
                                 iconColor="#fff">
-                        </AppButton>
-                    </Card>
-                </View>
+                            </AppButton>
+                        </Card>
+                    </View>
+                </KeyboardAvoidingView>
             </BackgroundImage>
+
         )
     }
 }
 const styles = StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: 'rgba(231,228,224,0.8)'
-    }
+    },
 })
