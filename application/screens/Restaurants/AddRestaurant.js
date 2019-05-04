27a98f3b9325026205792e32467269d13b6788e1
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-05-04 09:22:19 -0500

    Avance sabado 9>20

--- application/screens/Restaurants/AddRestaurant.js
+++ application/screens/Restaurants/AddRestaurant.js
@@ -4,9 +4,40 @@ import {View, StyleSheet} from 'react-native';
 import BackgroundImage from '../../components/BackgroundImage';
 import { Card } from 'react-native-elements';
 import t from 'tcomb-form-native';
+import AppButton from '../../components/AppButton';
 const Form = t.form.Form;
+import * as firebase from 'firebase';
 
 export default class AddRestaurant extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            restaurant: {
+                nombre:'',
+                descripcion:'',
+                direccion:'',
+                capacidad:0,
+            }
+        }
+    }
+
+    actualizarState(data){
+        this.setState({
+            restaurant:data
+        })
+    }
+    crearRestaurant(){
+        // getValue() retorna un JSON con la información del formulario
+        // SI y SOLO SI el formulario tiene los campos validados
+        // en caso contrario, retorna "null"
+        const validador = this.refs.form.getValue();
+        if(validador){
+            const pk = firebase.database().ref().child('restaurants').push().key;
+            
+            
+        }
+    }
     render() {
         return (
             <BackgroundImage source={require('./../../../assets/images/bg1.jpg')}>
@@ -15,9 +46,19 @@ export default class AddRestaurant extends Component {
                         <View>
                             <Form ref="form"
                                     type={Restaurant}
-                                    options={options}>
+                                    options={options}
+                                    value={this.state.restaurant}
+                                    onChange={(data)=>{this.actualizarState(data)}}>
                             </Form>
-                        </View>
+                        </View>  
+                        <AppButton
+                                bgColor="rgba(255,38,74,0.9)"
+                                title="Agregar"
+                                action={this.crearRestaurant.bind(this)}
+                                iconName="plus"
+                                iconSize={30}
+                                iconColor="#fff">
+                        </AppButton>
                     </Card>
                 </View>
             </BackgroundImage>
